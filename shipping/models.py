55549eb069cb6d39aace6c4774970c2305534aed
Anna Greaves <gilhespy.anna@gmail.com>
2019-10-11 14:13:59 +0200

    Add shipping profiles model.

---
+++ shipping/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Shipping_profile(models.Model):
+    name = models.CharField(max_length=100)
+    price_nl = models.DecimalField(max_digits=6, decimal_places=2)
+    price_eu = models.DecimalField(max_digits=6, decimal_places=2)
+    price_non_eu = models.DecimalField(max_digits=6, decimal_places=2)
