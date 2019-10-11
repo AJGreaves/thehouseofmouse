3beea6589b0d3bebf9d800fca6464be1512e310c
Anna Greaves <gilhespy.anna@gmail.com>
2019-10-11 16:07:09 +0200

    Re-apply models for products, categories and shipping profiles.

--- shipping/admin.py
+++ shipping/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Shipping_profile
 
 # Register your models here.
+admin.site.register(Shipping_profile)
--- shipping/models.py
+++ shipping/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
 # Create your models here.
+class Shipping_profile(models.Model):
+    name = models.CharField(max_length=100)
+    price_nl = models.DecimalField(max_digits=6, decimal_places=2)
+    price_eu = models.DecimalField(max_digits=6, decimal_places=2)
+    price_non_eu = models.DecimalField(max_digits=6, decimal_places=2)
+    def __str__(self):
+        return self.name
