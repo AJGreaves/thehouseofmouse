1b92741c229902013cfd95daf7b7b7b35691fa26
Anna Greaves <gilhespy.anna@gmail.com>
2019-10-13 19:26:48 +0200

    Add basic template for listing page. Add quantity buttons.

--- static/js/main.js
+++ static/js/main.js
@@ -4,4 +4,35 @@
 $( document ).ready(function() {
     let date = new Date();
     $('#copyright-year').text( '2008 - ' + date.getFullYear())
+
+    /**
+     * Function to increment value in listing quantity input fields
+     * Credit: http://jsfiddle.net/laelitenetwork/puJ6G/
+     */
+    $('.qtyplus').click(function(e){
+        e.preventDefault();
+        fieldName = $(this).attr('field');
+        var currentVal = parseInt($('input[name='+fieldName+']').val());
+        if (!isNaN(currentVal)) {
+            $('input[name='+fieldName+']').val(currentVal + 1);
+        } else {
+            $('input[name='+fieldName+']').val(0);
+        }
+    });
+
+    /**
+     * Function to decrement value in listing quantity input fields
+     */
+    $(".qtyminus").click(function(e) {
+        e.preventDefault();
+        fieldName = $(this).attr('field');
+        var currentVal = parseInt($('input[name='+fieldName+']').val());
+        if (!isNaN(currentVal) && currentVal > 0) {
+            $('input[name='+fieldName+']').val(currentVal - 1);
+        } else {
+            $('input[name='+fieldName+']').val(0);
+        }
+    });
+    /**End credit */
+
 });
