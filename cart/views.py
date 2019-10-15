d6d63e07e22ac3ba47c71315f932a5abbaa71553
Anna Greaves <gilhespy.anna@gmail.com>
2019-10-15 11:32:29 +0200

    Create test cart.html template.

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def cart_view(request, *args, **kwargs):
+    return render(request, "cart.html")
--- thehouseofmouse/urls.py
+++ thehouseofmouse/urls.py
@@ -17,13 +17,14 @@ from django.contrib import admin
 from django.urls import path
 from pages.views import home_view, faqs_view, about_view
 from products.views import listing_view, results_view
-# results_view can be used for search results, category or favourites
+from cart.views import cart_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name="home"),
     path('listing/', listing_view, name="listing"),
-    path('results/', results_view, name="listing"),
+    path('results/', results_view, name="results"),
     path('about/', about_view, name="about"),
+    path('cart/', cart_view, name="cart"),
     path('faqs/', faqs_view, name="faqs"),
 ]
