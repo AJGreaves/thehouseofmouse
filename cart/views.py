4c85e9c1d16fd8702aded5b32c3484b0399a3240
Anna Greaves <gilhespy.anna@gmail.com>
2019-10-15 13:53:32 +0200

    Add all checkout html pages (no content), views and urls.

--- cart/views.py
+++ cart/views.py
@@ -2,4 +2,20 @@ from django.shortcuts import render
 
 # Create your views here.
 def cart_view(request, *args, **kwargs):
-    return render(request, "cart.html")
+    """ 
+    render shopping cart page, remove footer from this page 
+    to fit conventions of other eCommerce sites 
+    """
+    return render(request, "cart.html", {"footer": False})
+
+def checkout1_view(request, *args, **kwargs):
+    return render(request, "checkout1.html")
+
+def checkout2_view(request, *args, **kwargs):
+    return render(request, "checkout2.html")
+
+def checkout3_view(request, *args, **kwargs):
+    return render(request, "checkout3.html")
+
+def checkout4_view(request, *args, **kwargs):
+    return render(request, "checkout4.html")
--- thehouseofmouse/urls.py
+++ thehouseofmouse/urls.py
@@ -17,14 +17,25 @@ from django.contrib import admin
 from django.urls import path
 from pages.views import home_view, faqs_view, about_view
 from products.views import listing_view, results_view
-from cart.views import cart_view
+from cart.views import cart_view, checkout1_view, checkout2_view, checkout3_view, checkout4_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # standard pages
     path('', home_view, name="home"),
+    path('faqs/', faqs_view, name="faqs"),
+    path('about/', about_view, name="about"),
+
+    # listings and search
     path('listing/', listing_view, name="listing"),
     path('results/', results_view, name="results"),
-    path('about/', about_view, name="about"),
+
+    # cart and payment
     path('cart/', cart_view, name="cart"),
-    path('faqs/', faqs_view, name="faqs"),
+    path('checkout/info', checkout1_view, name="info"),
+    path('checkout/shipping', checkout2_view, name="shipping"),
+    path('checkout/payment', checkout3_view, name="payment"),
+    path('checkout/confirm', checkout4_view, name="confirm"),
+
 ]
