56e87b2804573d906cdc4ba893802052ff29839b
Anna Greaves <gilhespy.anna@gmail.com>
2019-10-16 10:46:00 +0200

    Checkout progress bar displaying correctly on each step.

--- cart/views.py
+++ cart/views.py
@@ -2,15 +2,6 @@ from django.shortcuts import render
 
 # Context objects
 
-"""
-Removes navbar and footer for checkout process, as
-is convention for online stores to guide buyers to final purchase
-"""
-nav_and_footer = {
-    "footer": False,
-    "navbar": False
-}
-
 
 # Create your views here.
 def cart_view(request, *args, **kwargs):
@@ -21,14 +12,33 @@ def cart_view(request, *args, **kwargs):
     return render(request, "cart.html", {"footer": False})
 
 def checkout1_view(request, *args, **kwargs):
-
-    return render(request, "checkout1.html", nav_and_footer)
+    context = {
+        "footer": False,
+        "navbar": False,
+        "active_pg": "checkout_info"
+    }
+    return render(request, "checkout1.html", context)
 
 def checkout2_view(request, *args, **kwargs):
-    return render(request, "checkout2.html", nav_and_footer)
+    context = {
+        "footer": False,
+        "navbar": False,
+        "active_pg": "checkout_shipping"
+    }
+    return render(request, "checkout2.html", context)
 
 def checkout3_view(request, *args, **kwargs):
-    return render(request, "checkout3.html", nav_and_footer)
+    context = {
+        "footer": False,
+        "navbar": False,
+        "active_pg": "checkout_payment"
+    }
+    return render(request, "checkout3.html", context)
 
 def checkout4_view(request, *args, **kwargs):
-    return render(request, "checkout4.html", nav_and_footer)
+    context = {
+        "footer": False,
+        "navbar": False,
+        "active_pg": "checkout_confirm"
+    }
+    return render(request, "checkout4.html", context)
