1d78d6c401448429362883c8da4753c9b52ccdac
Anna Greaves <gilhespy.anna@gmail.com>
2019-10-16 11:17:30 +0200

    Add show order summary dropdown for checkout.

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,21 @@
-/**
- * Sets copyright date in footer to current year
- */
-$( document ).ready(function() {
+$(document).ready(function () {
+
+    /**
+     * Sets copyright date in footer to current year
+     */
     let date = new Date();
-    $('#copyright-year').text( '2008 - ' + date.getFullYear())
+    $('#copyright-year').text('2008 - ' + date.getFullYear())
 
-
+    /**
+     * Changes direction of chevron when clicked
+     */
+    $('.chevron').click(function() {
+        if ($('.chevron').hasClass('fa-chevron-down')) {
+            $('.chevron').addClass('fa-chevron-up').removeClass('fa-chevron-down')
+        } else {
+            $('.chevron').addClass('fa-chevron-down').removeClass('fa-chevron-up')
+        }
+        
+    })
 
 });
