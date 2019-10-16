9c14dfd843a00f66eb52c79dcdb003d0ec0fedc4
Anna Greaves <gilhespy.anna@gmail.com>
2019-10-16 07:02:01 +0200

    Write logic to remove navbar and footer from checkout pages.

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
+# Context objects
+
+"""
+Removes navbar and footer for checkout process, as
+is convention for online stores to guide buyers to final purchase
+"""
+nav_and_footer = {
+    "footer": False,
+    "navbar": False
+}
+
+
 # Create your views here.
 def cart_view(request, *args, **kwargs):
     """ 
@@ -9,13 +21,14 @@ def cart_view(request, *args, **kwargs):
     return render(request, "cart.html", {"footer": False})
 
 def checkout1_view(request, *args, **kwargs):
-    return render(request, "checkout1.html")
+
+    return render(request, "checkout1.html", nav_and_footer)
 
 def checkout2_view(request, *args, **kwargs):
-    return render(request, "checkout2.html")
+    return render(request, "checkout2.html", nav_and_footer)
 
 def checkout3_view(request, *args, **kwargs):
-    return render(request, "checkout3.html")
+    return render(request, "checkout3.html", nav_and_footer)
 
 def checkout4_view(request, *args, **kwargs):
-    return render(request, "checkout4.html")
+    return render(request, "checkout4.html", nav_and_footer)
