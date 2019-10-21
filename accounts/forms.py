9221d5e4749486a2a6205e0a15af02a0f4a5cc3a
Anna Greaves <gilhespy.anna@gmail.com>
2019-10-21 14:44:26 +0200

    Create class UserRegistrationForm in accounts app.

---
+++ accounts/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class UserRegistrationForm(UserCreationForm):
+    """
+    Default django user creation form but make email address field required.
+    example code used for reference by fellow student Robin Zigmond
+    https://github.com/robinzigmond/chamber_mates/blob/master/accounts/forms.py
+    """
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
+
+    def __init__(self, *args, **kwargs):
+        super(UserRegistrationForm, self).__init__(*args, **kwargs)
+        self.fields["email"].required = True
+
