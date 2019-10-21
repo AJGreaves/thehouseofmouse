b05cc390a8b0871bccaf6e91fa5aace007e66247
Anna Greaves <gilhespy.anna@gmail.com>
2019-10-21 11:47:34 +0200

    Adjust view for product listing to import data from database.

--- products/views.py
+++ products/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 def listing_view(request, *args, **kwargs):
-    return render(request, "listing.html")
+    product = Product.objects.get(id=1)
+    context = {
+        'title': product.title,
+        'description': product.description,
+        'price': product.price,
+        'category': product.category,
+        'num_in_stock': product.num_in_stock,
+    }
+    return render(request, "listing.html", context)
 
 def results_view(request, *args, **kwargs):
     """ results_view can be used for search results, category or favourites """
