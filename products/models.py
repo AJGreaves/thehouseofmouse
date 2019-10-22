6806a2177ba19f1d5ba1c3f918f92e6a10ddf2eb
Anna Greaves <gilhespy.anna@gmail.com>
2019-10-22 15:07:52 +0200

    Add featured field to product model.

--- products/models.py
+++ products/models.py
@@ -39,6 +39,8 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     tags = models.CharField(max_length=300)
     num_in_stock = models.PositiveSmallIntegerField(validators=[MaxValueValidator(100)])
+    featured = models.BooleanField(default=False)
+
 
     def __str__(self):
         return self.title
