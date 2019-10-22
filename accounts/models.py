64786ac0dea331dad4c6c7430a469b797b754780
Anna Greaves <gilhespy.anna@gmail.com>
2019-10-22 15:22:51 +0200

    Create user profile model.

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from shipping.models import Destination_country
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address1 = models.CharField(max_length=150)
+    address2 = models.CharField(max_length=150, blank=True, null=True)
+    town = models.CharField(max_length=150)
+    postcode = models.CharField(max_length=10)
+    county = models.CharField(max_length=150, blank=True, null=True)
+    country = models.ForeignKey(
+        Destination_country,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
