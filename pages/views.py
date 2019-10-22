c14cd92521ad8339bf13844f2565fea8ee411a3d
Anna Greaves <gilhespy.anna@gmail.com>
2019-10-22 13:22:05 +0200

    Add about and faqs pages to navbar.

--- pages/views.py
+++ pages/views.py
@@ -2,13 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    context = {
-        "page": "home"
-    }
-    return render(request, "pages/index.html", context)
+    return render(request, "pages/index.html", {"page": "home"})
 
 def faqs_view(request, *args, **kwargs):
-    return render(request, "pages/faqs.html")
+    return render(request, "pages/faqs.html", {"page": "faqs"})
 
 def about_view(request, *args, **kwargs):
-    return render(request, "pages/about.html")
+    return render(request, "pages/about.html", {"page": "about"})
