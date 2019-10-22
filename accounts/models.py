5914eb3591966d26eb787686ab2a8984ec5ca263
Anna Greaves <gilhespy.anna@gmail.com>
2019-10-22 18:07:44 +0200

    Attempting to create new profile with new user. Not working yet.

--- accounts/models.py
+++ accounts/models.py
@@ -4,14 +4,15 @@ from shipping.models import Destination_country
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    address1 = models.CharField(max_length=150)
+    address1 = models.CharField(max_length=150, null=True)
     address2 = models.CharField(max_length=150, blank=True, null=True)
-    town = models.CharField(max_length=150)
+    town = models.CharField(max_length=150, null=True)
     county = models.CharField(max_length=150, blank=True, null=True)
-    postcode = models.CharField(max_length=10)
+    postcode = models.CharField(max_length=10, null=True)
     country = models.ForeignKey(
         Destination_country,
         on_delete=models.CASCADE,
+        null=True,
     )
 
     def __str__(self):
