2a58a63ddc441980e0d6516320dc40a22a248fe6
Anna Greaves <gilhespy.anna@gmail.com>
2019-10-23 11:59:49 +0200

    Update profile form for first and last name and style.

--- accounts/views.py
+++ accounts/views.py
@@ -33,8 +33,18 @@ def profile_view(request):
     their information, already filled out with their current
     data.
     """
-    u_form = UserUpdateForm(instance=request.user)
-    p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account info has been updated.')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
 
     context = {
         'u_form': u_form,
