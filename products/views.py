e8f839c17abb87a794e6efa6b7d1ff69dca1da80
Anna Greaves <gilhespy.anna@gmail.com>
2019-10-23 18:41:43 +0200

    Create ListingDetailView in place of listing_view function.

--- products/views.py
+++ products/views.py
@@ -3,25 +3,32 @@ from django.views.generic import DetailView
 from .models import Product
 
 # Create your views here.
-def listing_view(request, *args, **kwargs):
-    """ view specific product details """
-    product = Product.objects.get(id=1)
-    num_in_stock = product.num_in_stock
-
-    stock_arr = []
-    for i in range(num_in_stock):
-        stock_arr.append(i)
-
-    context = {
-        'product': product,
-        'stock_arr': stock_arr,
-    }
-    return render(request, "listing.html", context)
-
 class ListingDetailView(DetailView):
+    """ Create view for individual listings """
     model = Product
     template_name = 'listing.html'
     context_object_name = 'product'
+    
+
+    def get_context_data(self, **kwargs):
+        """
+        Get the current context and the relevant pk, then set the
+        context needed for building the select dropdown menu.
+        """
+        context = super().get_context_data(**kwargs)
+        pk = self.kwargs['pk']
+
+        product = Product.objects.get(id=pk)
+        num_in_stock = product.num_in_stock
+
+        stock_arr = []
+        for i in range(num_in_stock):
+            stock_arr.append(i)
+
+        context['product'] = product
+        context['stock_arr'] = stock_arr
+
+        return context
 
 def results_view(request, *args, **kwargs):
     """ results_view can be used for search results, category or favourites """
