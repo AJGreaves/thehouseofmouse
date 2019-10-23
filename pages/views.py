2df71da387c4e3e634d16d150cb589c38184d64f
Anna Greaves <gilhespy.anna@gmail.com>
2019-10-23 22:18:33 +0200

    Wire up featured listings to home page.

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from products.models import Product
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    return render(request, "pages/index.html", {"page": "home"})
+    featured_products = Product.objects.order_by('-featured', '?')[:6]
+    context = {
+        'featured_products': featured_products,
+        'category': 'All Products',
+        'page': 'home',
+    }
+    return render(request, "pages/index.html", context)
 
 def faqs_view(request, *args, **kwargs):
     return render(request, "pages/faqs.html", {"page": "faqs"})
