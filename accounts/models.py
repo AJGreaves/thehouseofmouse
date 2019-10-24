0b70c880a82c3e18df8fc421bf28f6632dd71de4
Anna Greaves <gilhespy.anna@gmail.com>
2019-10-24 11:12:18 +0200

    Remove commented out code.

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from shipping.models import Destination_country
-# from django.db.models.signals import post_save
-# from django.dispatch import receiver
-# from django.conf import settings
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
