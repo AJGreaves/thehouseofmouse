fb64c96240bf08cd501dd6621228104512f9e057
Anna Greaves <gilhespy.anna@gmail.com>
2019-10-28 09:42:35 +0100

    Create forms for order item and order form.

---
+++ cart/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models import Order, OrderItem
+
+class OrderItemForm(forms.ModelForm):
+
+    class Meta:
+        model = OrderItem
+        fields = ['order', 'product', 'quantity']
+
+class OrderForm(forms.ModelForm):
+
+    class Meta:
+        model = Order
+        fields = [
+            'full_name',
+            'address_line_1',
+            'address_line_2',
+            'town_or_city',
+            'county',
+            'country']
