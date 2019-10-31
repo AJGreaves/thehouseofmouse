9c3f227c480b09981a991e9b3f8150d6f85fdf42
Anna Greaves <gilhespy.anna@gmail.com>
2019-10-31 12:53:32 +0100

    Register cart models with admin page.

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import ShippingDestination, Order, OrderItem
 
 # Register your models here.
+admin.site.register(ShippingDestination)
+admin.site.register(Order)
+admin.site.register(OrderItem)
