253f6ca5aab342c12c6f405413cd2f27ca444a73
Anna Greaves <gilhespy.anna@gmail.com>
2019-11-04 09:49:49 +0100

    Create form for making payment.

--- cart/forms.py
+++ cart/forms.py
@@ -26,3 +26,13 @@ class NewOrderForm(forms.ModelForm):
             'postcode',
             'country',
         ]
+
+class MakePaymentForm(forms.Form):
+    MONTH_CHOICES = [(i, i) for i in range(1, 12)]
+    YEAR_CHOICES = [(i, i) for i in range(2019, 2040)]
+
+    credit_cart_number = forms.CharField(label='Credit card number', required=False)
+    cvv = forms.CharField(label="Security code (CVV)", required=False)
+    expiry_month = forms.ChoiceField(label="Month", choices=MONTH_CHOICES, required=False)
+    expiry_year = forms.ChoiceField(label="Year", choices=YEAR_CHOICES, required=False)
+    stripe_id = forms.CharField(widget=forms.HiddenInput)
