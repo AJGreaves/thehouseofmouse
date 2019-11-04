bb03df6c7c4c808acfa04847905c73f428637760
Anna Greaves <gilhespy.anna@gmail.com>
2019-11-04 18:45:42 +0100

    Demo line item for stripe working, items appearing in stripe dashboard.

--- cart/views.py
+++ cart/views.py
@@ -181,6 +181,20 @@ def checkout_payment_view(request, *args, **kwargs):
 
             # when stripe successful update order paid
             # return redirect('confirm')
+        
+        session = stripe.checkout.Session.create(
+            payment_method_types=['card'],
+            line_items=[{
+                'name': 'T-shirt',
+                'description': 'Comfortable cotton t-shirt',
+                'images': ['https://example.com/t-shirt.png'],
+                'amount': 500,
+                'currency': 'eur',
+                'quantity': 1,
+            }],
+            success_url='https://example.com/success?session_id={CHECKOUT_SESSION_ID}',
+            cancel_url='https://example.com/cancel',
+        )
 
         new_context = {
             **context,
