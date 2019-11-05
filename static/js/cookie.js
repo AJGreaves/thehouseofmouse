5d6b27c91150d33c122549c0a89b5a2deffc7953
Anna Greaves <gilhespy.anna@gmail.com>
2019-11-05 15:52:36 +0100

    Update js to let instead of var.

--- static/js/cookie.js
+++ static/js/cookie.js
@@ -4,11 +4,11 @@
  */
 
 function getCookie(name) {
-    var cookieValue = null;
+    let cookieValue = null;
     if (document.cookie && document.cookie !== '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = cookies[i].trim();
+        let cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            let cookie = cookies[i].trim();
             // Does this cookie string begin with the name we want?
             if (cookie.substring(0, name.length + 1) === (name + '=')) {
                 cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
@@ -20,4 +20,4 @@ function getCookie(name) {
 }
 
 // variable to use for fetch / ajax requests
-var csrftoken = getCookie('csrftoken');
+let csrftoken = getCookie('csrftoken');
