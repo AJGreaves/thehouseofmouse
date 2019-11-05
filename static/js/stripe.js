3d48837f131eedadfb0bc365588a151dd13bb333
Anna Greaves <gilhespy.anna@gmail.com>
2019-11-05 11:47:19 +0100

    Minor housekeeping in stripe.js

--- static/js/stripe.js
+++ static/js/stripe.js
@@ -1,6 +1,6 @@
-// // All code in this file provided by https://stripe.com/docs/payments/cards/collecting/web
-
 const s_pub = returnStripePublishableKey();
+
+// // All code below provided by https://stripe.com/docs/payments/cards/collecting/web
 
 let stripe = Stripe(s_pub);
 let elements = stripe.elements();
