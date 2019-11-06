6f9eca02dc1a804f7cd7e062e1d730b55d27d4a2
Anna Greaves <gilhespy.anna@gmail.com>
2019-11-06 10:11:52 +0100

    Add background image to account pages to push footer down.

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm, UserUpdateForm
-from cart.models import Order, OrderItem, ShippingDestination
+from cart.models import Order, OrderItem
 
 # Create your views here.
 def register_view(request):
