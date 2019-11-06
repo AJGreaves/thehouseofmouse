7942b9cf5edb3eb4da6a13b1c961bb4f46b02951
Anna Greaves <gilhespy.anna@gmail.com>
2019-11-06 11:21:48 +0100

    Update path for login page to redirect users already logged in back to the home page.

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from .views import register_view, profile_view
 
 urlpatterns = [
     path('register/', register_view, name="register"),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name="login"),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html', redirect_authenticated_user=True), name="login"),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name="logout"),
     path('profile/', profile_view, name="profile"),
 ]
