455132bd8dc3104cd444e80d2728879aa1d8eb78
Anna Greaves <gilhespy.anna@gmail.com>
2019-11-07 13:23:07 +0100

    Search view function working as expected.

--- search/urls.py
+++ search/urls.py
@@ -3,5 +3,4 @@ from .views import search_view
 
 urlpatterns = [
     path('', search_view, name='search'),
-    path('/<str:search_input>', search_view, name='search-results'),
 ]
--- search/views.py
+++ search/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import render
-from django.contrib.postgres.search import SearchVector
 from products.models import Product
+from django.db.models import Q #for multiple searchs
 
 # Create your views here.
-def search_view(request, q='', *args, **kwargs):
-    results = Product.objects.annotate(
-        search=SearchVector('title', 'description', 'tags')
-    ).filter(search=q)
 
+def search_view(request, *args, **kwargs):
+
+    products = Product.objects.all().order_by('-featured')
     context = {
-        'products': results,
-        'category': 'search',
-        'search_params': q
+        'products': products,
+        'category': 'Search'
     }
+
+    if request.method == 'POST':
+        query = request.POST.get('q')
+        results = Product.objects.filter(Q(title__icontains=query) | Q(description__icontains=query) | Q(tags__icontains=query))
+        context = {
+            'products': results,
+            'category': 'Search',
+            'search_params': query
+        }
+
+        return render(request, "results.html", context)
 
     return render(request, "results.html", context)
