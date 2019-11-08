34fb30c7b36bf82c7c56bdfbb66bf2b0f36938a8
Anna Greaves <gilhespy.anna@gmail.com>
2019-11-08 10:45:09 +0100

    Add url for search results page.

--- search/urls.py
+++ search/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import search_view
+from .views import search_view, search_results_view
 
 urlpatterns = [
     path('', search_view, name='search'),
+    path('<slug:query>', search_results_view, name='search-results')
 ]
