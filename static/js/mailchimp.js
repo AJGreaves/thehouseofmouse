a53619a1ebddd3b02496d48c09a3ac52b03c5b74
Anna Greaves <gilhespy.anna@gmail.com>
2019-11-08 12:54:01 +0100

    Remove mailchimp signup form.

--- static/js/mailchimp.js
+++
@@ -1,9 +0,0 @@
-(function ($) {
-    window.fnames = new Array();
-    window.ftypes = new Array();
-    fnames[0] = 'EMAIL';
-    ftypes[0] = 'email';
-    fnames[1] = 'MMERGE1';
-    ftypes[1] = 'text';
-}(jQuery));
-var $mcj = jQuery.noConflict(true);
