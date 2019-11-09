9380ae40c6bff12fc0cef2793c6d7d09322458c0
Anna Greaves <gilhespy.anna@gmail.com>
2019-11-09 17:45:43 +0100

    change STATIC_URL from static to staticfiles for deployment.

--- thehouseofmouse/settings.py
+++ thehouseofmouse/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
