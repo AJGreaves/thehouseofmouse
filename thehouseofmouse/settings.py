db27454658adb6cb2d0856a6543ece5ce2773856
Anna Greaves <gilhespy.anna@gmail.com>
2019-11-09 17:51:54 +0100

    Add if statement for STATIC_URL.

--- thehouseofmouse/settings.py
+++ thehouseofmouse/settings.py
@@ -132,7 +132,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/staticfiles/'
+if 'HEROKU' in os.environ:
+    STATIC_URL = '/staticfiles/'
+else:
+    STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
