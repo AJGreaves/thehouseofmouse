bc389f60f94b31815d5e4d0dab623754d66a2f3a
Anna Greaves <gilhespy.anna@gmail.com>
2019-11-10 08:54:45 +0100

    Set debug to true to solve 405 error on shipping page.

--- thehouseofmouse/settings.py
+++ thehouseofmouse/settings.py
@@ -25,10 +25,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if 'DEV' in os.environ:
-    DEBUG = True
-else:
-    DEBUG = False
+# if 'DEV' in os.environ:
+#     DEBUG = True
+# else:
+#     DEBUG = False
+
+DEBUG = True
 
 ALLOWED_HOSTS = [os.environ.get('HOSTNAME')]
 
