25256a680027b0eaa24a07c3591f6f58c86c0d98
Anna Greaves <gilhespy.anna@gmail.com>
2019-11-10 22:40:07 +0100

    Update build_search_url template tag to more robust function.

--- search/templatetags/search_extras.py
+++ search/templatetags/search_extras.py
@@ -4,11 +4,10 @@ register = template.Library()
 
 @register.filter(name='build_search_url')
 def build_search_url(absolute_url):
-    """ 
-    Custom tag for pagination to filter 
+    """
+    Custom tag for pagination to filter
     out the previous page from absolute url
     """
     if '&page=' in absolute_url:
-        absolute_url = absolute_url[:-7]
+        absolute_url = absolute_url.split('&page=')[0]
     return absolute_url
-    
