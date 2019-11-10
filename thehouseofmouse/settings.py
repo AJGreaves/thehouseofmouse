636c1cd472e6c64e638ef09b4752519501cf36d5
Anna Greaves <gilhespy.anna@gmail.com>
2019-11-10 10:30:31 +0100

    Comment debug false if statement for deployment back in.

--- thehouseofmouse/settings.py
+++ thehouseofmouse/settings.py
@@ -25,12 +25,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# if 'DEV' in os.environ:
-#     DEBUG = True
-# else:
-#     DEBUG = False
-
-DEBUG = True
+if 'DEV' in os.environ:
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = [os.environ.get('HOSTNAME')]
 
