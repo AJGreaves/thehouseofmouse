29f35040f7213db0463db8ccb8733fc130250b10
Anna Greaves <gilhespy.anna@gmail.com>
2019-11-11 17:15:53 +0100

    Write first test for OrderItem model.

--- cart/tests/test_models.py
+++ cart/tests/test_models.py
@@ -1,9 +1,9 @@
 from django.test import TestCase
 from cart.models import ShippingDestination, Order, OrderItem
+from products.models import Product
 
 
 class ShippingDestinationEntryTest(TestCase):
-    """ Test ShippingDestination model returns country string """
     def test_string_representation(self):
         sd = ShippingDestination(country="Narnia")
         self.assertEqual(str(sd), sd.country)
@@ -21,13 +21,11 @@ class ShippingDestinationEntryTest(TestCase):
         self.assertEqual(sd.shipping_price, 42)
         self.assertNotEqual(sd.shipping_price, 99.99)
 
+class OrderItemEntryTest(TestCase):
 
-    # def test_get_absolute_url(self):
-    #     pk = 99
-    #     title = "Test Mouse"
-    #     expected_result = '/products/listing/' + str(pk) + '/' + slugify(title)
-
-    #     product = Product(pk=pk, title=title)
-    #     result = Product.get_absolute_url(product)
-
-    #     self.assertEqual(result, expected_result)
+    def test_string_representation(self):
+        product = Product(title="Test Mouse")
+        order = Order(full_name="Arthur Dent")
+        order_item = OrderItem(order=order, product=product, quantity=3)
+        expected_result = '3 x Test Mouse'
+        self.assertEqual(str(order_item), expected_result)
