4b066c7e3b9a25e09cc31369298f96b2a8a11c81
Anna Greaves <gilhespy.anna@gmail.com>
2019-11-11 11:11:52 +0100

    Comment out incomplete test. Debugging testing issue.

--- products/tests/test_urls.py
+++ products/tests/test_urls.py
@@ -1,9 +1,9 @@
-from django.test import SimpleTestCase
-from django.urls import reverse, resolve
-from products.views import ListingDetailView
+# from django.test import SimpleTestCase
+# from django.urls import reverse, resolve
+# from products.views import ListingDetailView
 
-class TestUrls(SimpleTestCase):
+# class TestUrls(SimpleTestCase):
 
-    def test_listing_url_is_resolved(self):
-        url = reverse('listing-detail')
-        print(resolve(url))
+#     def test_listing_url_is_resolved(self):
+#         url = reverse('listing-detail')
+#         print(resolve(url))
