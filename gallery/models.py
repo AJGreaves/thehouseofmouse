6bdcc6811207c2276a3267f301e8002d80b69bbb
Anna Greaves <gilhespy.anna@gmail.com>
2019-11-15 13:35:15 +0100

    Change upload directory for gallery images.

--- gallery/models.py
+++ gallery/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class GalleryItem(models.Model):
     """ Model for gallery images and descriptions """
     name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to="product_images")
+    image = models.ImageField(upload_to="gallery")
     description = models.TextField()
 
     def __str__(self):
