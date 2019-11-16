0e24c37a806439ac46f96db69041d06b7a863e9f
Anna Greaves <gilhespy.anna@gmail.com>
2019-11-16 11:00:02 +0100

    Add view and url for gallery page.

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import GalleryItem
 
-# Create your views here.
+class GalleryView(ListView):
+    model = GalleryItem
+    template_name = 'gallery.html'
+    context_object_name = 'gallery_items'
+    paginate_by = 24
--- thehouseofmouse/urls.py
+++ thehouseofmouse/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('cart/', include('cart.urls')),
     path('search/', include('search.urls')),
+    path('gallery/', include('gallery.urls')),
 
     path('admin/', admin.site.urls),
 ]
