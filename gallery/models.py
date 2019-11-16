6da2fee74d5f5aaec1d2a533878d71d920643c3b
Anna Greaves <gilhespy.anna@gmail.com>
2019-11-16 10:48:26 +0100

    Make description field optional.

--- gallery/models.py
+++ gallery/models.py
@@ -4,7 +4,7 @@ class GalleryItem(models.Model):
     """ Model for gallery images and descriptions """
     name = models.CharField(max_length=100)
     image = models.ImageField(upload_to="gallery")
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
